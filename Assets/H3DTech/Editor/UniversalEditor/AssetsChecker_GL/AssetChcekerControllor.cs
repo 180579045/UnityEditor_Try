871e0d669000e7fb27a23dd75986f4d38e469c0a
180579045 <msn103125@126.com>
2016-03-03 21:44:37 +0800

    新建一个资源检查工具，尝试编写代码

---
+++ Assets/H3DTech/Editor/UniversalEditor/AssetsChecker_GL/AssetChcekerControllor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using YamlDotNet.RepresentationModel;
+using System.IO;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+public class AssetChcekerControllor{
+
+    static EditorRoot s_root = null;
+
+    [MenuItem("GL/资源检查工具2")]
+    static void Init()
+    {
+        //一个单例模式，UI框架只允许同时实例化一个同名UI;
+        EditorRoot root = EditorManager.GetInstance().FindEditor("资源检查工具2");
+        if (root == null)
+        {
+            root = EditorManager.GetInstance().CreateEditor("资源检查工具2", false, InitControls);
+        }
+    }
+
+    public static void InitControls(EditorRoot root)
+    {
+        
+    }
+}
